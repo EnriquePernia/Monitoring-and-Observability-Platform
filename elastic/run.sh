90ad4465ed351096fd163e8a2e522ecd319f011b
EnriquePernia <Epernia96@gmail.com>
2021-10-26 22:13:22 +0200

    Updated run.sh and modified elasticsearch not to use snapshots

--- elastic/run.sh
+++ elastic/run.sh
@@ -5,9 +5,6 @@ aws eks update-kubeconfig --name eks-cluster
 kubectl create -f https://download.elastic.co/downloads/eck/1.8.0/crds.yaml
 kubectl apply -f https://download.elastic.co/downloads/eck/1.8.0/operator.yaml
 
-kubectl create ns monitoring
-kubectl config set-context --current --namespace monitoring
-
 kubectl apply -f .
 sleep 60
 kubectl port-forward service/kibana-kb-http 5601
